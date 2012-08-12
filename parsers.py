03f9bddaa20c53bc86374b307ae8354b22b84cc6
Jeff Parsons <jeff@tony>
2012-08-12 22:46:25 +1000

    Split parsers out into separate file.

---
+++ parsers.py
@@ -0,0 +1,34 @@
+import re
+from heredoc import HeredocStream
+
+def parse(stream, out):
+	for line in stream:
+		if re.match('#.*', line):
+			# Ignore comment.
+			continue
+		match = re.match('@parser (\w+)$', line)
+		if match:
+			parser_name = match.group(1)
+			globals()[parser_name](stream, out)
+			continue
+		match = re.match('@parser (\w+) until (.*)$', line)
+		if match:
+			parser_name = match.group(1)
+			delimiter = match.group(2)
+			heredoc = HeredocStream(stream, delimiter)
+			globals()[parser_name](heredoc, out)
+			continue
+		# Pass everything else through verbatim.
+		out.writelines(line)
+
+# Sample alternative parser.
+def block_quoter(stream, out):
+	for line in stream:
+		if line == '<\n':
+			return
+		else:
+			out.writelines("> " + line)
+
+def dumb_block_quoter(stream, out):
+	for line in stream:
+		out.writelines("> " + line)
